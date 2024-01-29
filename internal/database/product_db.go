b42f369906f2fb5d55c0e4369b5bb93876d12738
Gabriel Brito de França <gabrielbfranca@gmail.com>
2024-01-29 20:46:32 -0300

    criação da base de dados e começo da camada de serviço

--- internal/database/product_db.go
+++ internal/database/product_db.go
@@ -1,6 +1,10 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/devfullcycle/imersao17/goapi/internal/entity"
+)
 
 type ProductDB struct {
 	db *sql.DB
@@ -8,4 +12,58 @@ type ProductDB struct {
 
 func NewProductDB(db *sql.DB) *ProductDB {
 	return &ProductDB{db: db}
+}
+
+func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
+	rows, err := pd.db.Query("select id, name, price, category_id from products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*entity.Product
+	for rows.Next() {
+		var product entity.Product
+		if err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.CategoryID); err != nil {
+			return nil, err
+		}
+		products = append(products, &product)
+	}
+	return products, nil
+}
+
+func (pd *ProductDB) GetProduct(id string) (*entity.Product, error) {
+	var product entity.Product
+	err := pd.db.QueryRow("SELECT id, name, price, category_id, image_url FROM product WHERE product id = ?", id).Scan(&product.ID, &product.Name, &product.Price, &product.CategoryID, &product.ImageURL)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+func (pd *ProductDB) GetProductByCategoryID(categoryID string) ([]*entity.Product, error) {
+	rows, err := pd.db.Query("select id, name, description, price, category_id, image_url from product where category_id=?", categoryID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*entity.Product
+	for rows.Next() {
+		var product entity.Product
+		if err := rows.Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL); err != nil {
+			return nil, err
+		}
+		products = append(products, &product)
+	}
+	return products, nil
+}
+
+func (pd *ProductDB) CreateProduct(product *entity.Product) (*entity.Product, error) {
+	_, err := pd.db.Exec("INSERT INTO product (id, name, description, price, category_id, image_url) values (?, ?, ?, ?, ?. ?)",
+		product.ID, product.Name, product.Description, product.Price, product.CategoryID, product.ImageURL)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
 }
---
+++ internal/service/category_service.go
@@ -0,0 +1,7 @@
+package service
+
+import "github.com/devfullcycle/imersao17/goapi/internal/database"
+
+type CategoryService struct {
+	CategoryDB database.CategoryDB
+}
